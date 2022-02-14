102121a0a773f65ecb9df4be21f28575582d5722
WooAf <info@phaonix.com>
2022-02-14 10:44:39 -0500

    Update node_exporter.sh

--- node_exporter.sh
+++ node_exporter.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-version="${VERSION:-1.3.1}"
-arch="${ARCH:-linux-amd64}"
 bin_dir="${BIN_DIR:-/usr/local/bin}"
 
-wget "https://github.com/prometheus/node_exporter/releases/download/v$version/node_exporter-$version.$arch.tar.gz" \
+wget "https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz" \
     -O /tmp/node_exporter.tar.gz
 
 mkdir -p /tmp/node_exporter
@@ -13,7 +11,7 @@ cd /tmp || { echo "Erreur! Aucun dossier /tmp trouvé.."; exit 1; }
 
 tar xfz /tmp/node_exporter.tar.gz -C /tmp/node_exporter || { echo "ERREUR! Extracting avec tar"; exit 1; }
 
-cp "/tmp/node_exporter/node_exporter-$version.$arch/node_exporter" "$bin_dir"
+cp "/tmp/node_exporter/node_exporter-1.3.1.linux-amd64/node_exporter" "$bin_dir"
 chown root:staff "$bin_dir/node_exporter"
 
 cat <<EOF > /etc/systemd/system/node_exporter.service
